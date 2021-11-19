8c7dc19d2c8307bbd0822902153fce5cef4cb581
Aleksandr <a13x.student@yandex.ru>
2021-11-19 14:52:52 +0300

    request test

--- src/routes/webHookRout.ts
+++ src/routes/webHookRout.ts
@@ -1,9 +1,15 @@
+import axios from 'axios'
 import { Router } from 'express'
 import { handleNlpRequest } from '../scenario'
 
 export const webHookRout = Router()
 
 webHookRout.post('/api/hook', async (req, res) => {
+    const {data} = await axios.post('https://cccstore.ru/api_test/token/', {
+            "client_login": "test_sberdevices",
+            "client_password": "test_mfnGnTJAkeXyEK",
+        })
+    console.log('data', data)
     console.log('api/hook POST request')
     res.status(200).json(await handleNlpRequest(req.body))
 })
